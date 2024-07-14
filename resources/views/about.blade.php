6a9be0b33e8197b1c4d8dd22e5f3c69ba13cf586
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-07-14 21:56:10 +0200

    first commit calrw

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>calrw | about page</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+	<h2>Hey there!</h2><hr>
+  <p>This is about page from controller</p>
+  <a href="{{route('contact.page')}}">Contact</a>
+  </body>
+</html>
