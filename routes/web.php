06e1f5b7f29df64815f15f33610dcd8695ee08e5
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-07-22 21:51:04 +0200

    User Profile added

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/', function () {
 // admin all routers
  Route::controller(AdminController::class)->group(function(){
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
 
  });
     
