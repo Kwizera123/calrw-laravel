989c62c5dcb05ca43bdaaeb93251ba449d80a326
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-07-24 13:48:54 +0200

    Admin Profile Change Password added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -62,5 +63,30 @@ class AdminController extends Controller
 
         return redirect()->route('admin.profile')->with($notification);
         
+    }// End method
+
+    public function ChangePassword(){
+        return view('admin.admin_change_password');
+    }// End method
+
+    public function UpdatePassword(Request $request){
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if(Hash::check($request->oldpassword,$hashedPassword)){
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message','Password Updated Successfully');
+            return redirect()->back();
+        }else{
+            session()->flash('message','Old Password does not match');
+            return redirect()->back();
+        }
     }// End method
 }
